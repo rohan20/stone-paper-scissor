97f4b3552c8fd583497f3566d3fae21e5a7a55ee
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-01-20 11:58:54 +0530

    comparing strings

--- rock-paper-scissor.rb
+++ rock-paper-scissor.rb
@@ -1,5 +1,24 @@
 print "Enter r for rock, p for paper and s for scissors\n"
 user_choice = gets
 
-print "\nYou chose: " + user_choice + "\n"
+print "\nYou chose: " + user_choice
+
+computer_choice_random = Random.rand(3)
+
+if computer_choice_random == 0
+	computer_choice = "r"
+elsif computer_choice_random == 1
+		computer_choice = "p"
+else 
+	computer_choice = "s"
+end
+
+print "Computer chose: " + computer_choice
+
+if user_choice.chomp == computer_choice.chomp
+	print "\nDraw\n\n"
+else 
+	print "\nNot draw\n\n"
+end
+
 
